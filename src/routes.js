6870adaf8a42267a3b492e079d36b7a8f6f96c4a
KevenJunco <49709096+KevenJunco@users.noreply.github.com>
2021-11-28 03:30:45 -0300

    paginacão de projetos

--- src/routes.js
+++ src/routes.js
@@ -2,14 +2,19 @@ const express = require('express')
 const routes = express.Router();
 
 const UserController = require('./controllers/UserController');
+const ProjectController = require('./controllers/ProjectController');
 
-routes.get('/users', UserController.index);
 
-routes.post('/users', UserController.create);
+routes
+.get('/users', UserController.index)
+.post('/users', UserController.create)
+.delete('/users/:id', UserController.delete)
+.put('/users/:id', UserController.update)
 
-routes.delete('/users/:id', UserController.delete)
-
-routes.put('/users/:id', UserController.update)
+.get('/projects', ProjectController.index)
+.post('/projects', ProjectController.create)
+.delete('/projects/:id', ProjectController.delete)
+.put('/projects/:id', ProjectController.update)
 
 
 
